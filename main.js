5d73f52d2f317b603fdcc61db3a383a6ff9941df
RUAN BRÖERING RODRIGUES <ruan_rodrigues3@estudante.sesisenai.org.br>
2025-05-07 08:35:31 -0300

    ts2

---
+++ main.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+
+function updateLog(){
+    fs.readFileSync('log.txt', 'utf-8', (err,data) =>{
+
+    })
+}
+const dados = [
+    
+]
+
+function writeInLog(){
+    fs.writeFileSync('log.txt', 'utf-8', (err,data)=> {
+
+    })
+}
+printLogs()
